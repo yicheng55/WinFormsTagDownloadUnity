1680f47abdf980a3cd7b265c6e29a90d74df0f95
yicheng55 <l5977951@gmail.com>
2021-07-24 10:41:55 +0800

    InputField test.

--- Assets/Script/Download.cs
+++ Assets/Script/Download.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Download : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public InputField textTest;
+    int score;
+
     public void ExitButton()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         SceneManager.LoadScene(0);
 
+    }
+
+    public void TestButton()
+    {
+        Debug.Log(textTest.text);
+        score++;
+        textTest.text = score.ToString();
+        Debug.Log(score);
     }
 }
