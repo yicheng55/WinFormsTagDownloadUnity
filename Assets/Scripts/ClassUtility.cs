dac2cb41151c579060bb44cf8b2eae886be1a585
yicheng55 <l5977951@gmail.com>
2021-07-25 15:00:48 +0800

    Update code.

---
+++ Assets/Scripts/ClassUtility.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using System.Windows.Forms;     //for MessageBox
+using System.Diagnostics;
+using UnityEditor;
+
+namespace WinFormsTagDownload
+{
+    public class TClassUtility
+    {
+        //private Form Owner;
+        static int RunCount = 10000;
+
+        //建構子
+        //public TClassUtility(Form Owner)
+        //{
+        //    //會跟著釋放
+        //    this.Owner = Owner;
+        //    if (Owner == null)
+        //    {
+        //        Owner = new Form();
+        //    }
+        //}
+
+
+
+        static void ListTest()
+        {
+            List<byte> byteSource = new List<byte>();
+            byteSource.Add(11);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < RunCount; i++)
+            {
+                byte[] newData = new byte[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                byteSource.AddRange(newData);
+            }
+            byte[] data = byteSource.ToArray();
+            //byte[] subData = byteSource.Take(100).ToArray();//獲取前100個位元組
+            //byteSource.RemoveRange(0, 100);//取出後刪除
+            //byteSource.GetRange(100, 100);//從下標100開始取100個位元組
+            sw.Stop();
+            Console.WriteLine("ListTest " + sw.ElapsedMilliseconds + " 毫秒,陣列長度：" + data.Length);
+        }
+
+        static void ArrayCopyTest()
+        {
+            byte[] byteSource = new byte[1] { 11 };
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < RunCount; i++)
+            {
+                byte[] newData = new byte[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                byte[] tmp = new byte[byteSource.Length + newData.Length];
+                Array.Copy(byteSource, tmp, byteSource.Length);
+                Array.Copy(newData, 0, tmp, byteSource.Length, newData.Length);
+                byteSource = tmp;
+            }
+            sw.Stop();
+            Console.WriteLine("ArrayCopyTest " + sw.ElapsedMilliseconds + " 毫秒,陣列長度：" + byteSource.Length);
+        }
+
+        static void BlockCopyTest()
+        {
+            byte[] byteSource = new byte[1] { 11 };
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < RunCount; i++)
+            {
+                byte[] newData = new byte[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                byte[] tmp = new byte[byteSource.Length + newData.Length];
+                System.Buffer.BlockCopy(byteSource, 0, tmp, 0, byteSource.Length);
+                System.Buffer.BlockCopy(newData, 0, tmp, byteSource.Length, newData.Length);
+                byteSource = tmp;
+            }
+            sw.Stop();
+            Console.WriteLine("BlockCopyTest " + sw.ElapsedMilliseconds + " 毫秒,陣列長度：" + byteSource.Length);
+        }
+
+
+        public void ArrayCopyTestFunction()
+        {
+            ArrayCopyTest();
+            BlockCopyTest();
+            ListTest();
+            Console.ReadKey();
+        }
+
+        public void DumpBytes(byte[] bdata, int len)
+        {
+            int i;
+            int j = 0;
+            char dchar;
+
+            // 3 * 16 chars for hex display, 16 chars for text and 8 chars
+            // for the 'gutter' int the middle.
+            StringBuilder dumptext = new StringBuilder("        ", 16 * 4 + 8);
+            for (i = 0; i < len; i++)
+            {
+                dumptext.Insert(j * 3, String.Format("{0:X2} ", (int)bdata[i]));
+                dchar = (char)bdata[i];
+                //' replace 'non-printable' chars with a '.'.
+                if (Char.IsWhiteSpace(dchar) || Char.IsControl(dchar))
+                {
+                    dchar = '.';
+                }
+                dumptext.Append(dchar);
+                j++;
+                if (j == 16)
+                {
+                    Console.WriteLine(dumptext);
+                    dumptext.Length = 0;
+                    dumptext.Append("        ");
+                    j = 0;
+                }
+            }
+            // display the remaining line
+            if (j > 0)
+            {
+                for (i = j; i < 16; i++)
+                {
+                    dumptext.Insert(j * 3, "   ");
+                }
+                Console.WriteLine(dumptext);
+            }
+        }
+
+        public string ToHexString(byte[] bytes)
+        {
+            string hexString = string.Empty;
+            if (bytes != null)
+            {
+                StringBuilder str = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    str.Append(bytes[i].ToString("X2"));
+                }
+                hexString = str.ToString();
+            }
+            return hexString;
+        }
+
+
+
+#if true
+
+        /// <summary>
+        /// Hex String 轉 byte[]
+        /// </summary>
+        /// <param name="newString">Hex String</param>
+        /// <returns>byte[]</returns>
+        public byte[] GetBytes(string HexString)
+        {
+            int byteLength = HexString.Length / 2;
+            byte[] bytes = new byte[byteLength];
+            string hex;
+            int j = 0;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hex = new String(new Char[] { HexString[j], HexString[j + 1] });
+                bytes[i] = HexToByte(hex);
+                j = j + 2;
+            }
+            return bytes;
+        }
+
+        public byte HexToByte(string hex)
+        {
+            if (hex.Length > 2 || hex.Length <= 0)
+                throw new ArgumentException("hex must be 1 or 2 characters in length");
+            byte newByte = byte.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+            return newByte;
+        }
+
+#endif
+
+
+        public static byte[] HexToByteArray(string hexString)
+        {
+            //運算後的位元組長度:16進位數字字串長/2
+            byte[] byteOUT = new byte[hexString.Length / 2];
+            for (int i = 0; i < hexString.Length; i = i + 2)
+            {
+                //每2位16進位數字轉換為一個10進位整數
+                byteOUT[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
+            }
+            return byteOUT;
+        }
+
+        public static string ByteArrayToHexString(byte[] Bdata)
+        {
+            return BitConverter.ToString(Bdata).Replace("-", "");
+        }
+
+        public string RunExeByProcess(string exePath, string argument)
+        {
+            //開啟新執行緒
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            //呼叫的exe的名稱
+            process.StartInfo.FileName = exePath;
+            //傳遞進exe的引數
+            process.StartInfo.Arguments = argument;
+            process.StartInfo.UseShellExecute = false;
+            //不顯示exe的介面
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.Start();
+
+            process.StandardInput.AutoFlush = true;
+
+            string result = null;
+            while (!process.StandardOutput.EndOfStream)
+            {
+                result += process.StandardOutput.ReadLine() + Environment.NewLine;
+            }
+            process.WaitForExit();
+            return result;
+        }
+
+        /////////*****************************************************
+        public int ExecuteCommand(string command)
+        {
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+            //MessageBoxButtons buttons = MessageBoxButtons.OK;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            string output = process.StandardOutput.ReadToEnd();   //要先輸出.若在畫面輸出的資料量大的時候，Powershell畫面所輸出的資料是會將Buffer寫爆的
+            process.WaitForExit();
+
+            // *** Read the streams ***
+            // Warning: This approach can lead to deadlocks, see Edit #2
+            //string output = process.StandardOutput.ReadToEnd();   //若在畫面輸出的資料量大的時候，Powershell畫面所輸出的資料是會將Buffer寫爆的
+            string error = process.StandardError.ReadToEnd();
+
+
+            exitCode = process.ExitCode;
+            if (output.Contains("Reset type NORMAL:"))
+            {
+                UnityEngine.Debug.Log("Can connect to target..........");
+
+            }
+            else
+            {
+                exitCode = 2;
+                error = "Fail....";
+                UnityEngine.Debug.Log("Cannot connect to target....");
+            }
+
+            //if (output.Contains("O.K"))
+            //{
+
+            //    Console.WriteLine("Download Flash OK....");
+            //}
+            //else
+            //{
+            //    exitCode = 2;
+            //    Console.WriteLine("Download Flash Fail..........");
+            //}
+
+            string message = String.Format("error = {0} \r\nexitCode = {1}", (String.IsNullOrEmpty(error) ? "(none)" : error), exitCode);
+            UnityEngine.Debug.Log("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+            UnityEngine.Debug.Log("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+            UnityEngine.Debug.Log("ExitCode: " + exitCode.ToString() + "ExecuteCommand");
+            //MessageBox.Show(message, "Show...", buttons);
+            process.Close();
+            return exitCode;
+        }
+
+
+        public int ExecuteCommandTest(string command)
+        {
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+            //MessageBoxButtons buttons = MessageBoxButtons.OK;
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            process = Process.Start(processInfo);
+            string output = process.StandardOutput.ReadToEnd();   //要先輸出.若在畫面輸出的資料量大的時候，Powershell畫面所輸出的資料是會將Buffer寫滿的
+            process.WaitForExit();
+
+            // *** Read the streams ***
+            // Warning: This approach can lead to deadlocks, see Edit #2
+            //string output = process.StandardOutput.ReadToEnd();   //若在畫面輸出的資料量大的時候，Powershell畫面所輸出的資料是會將Buffer寫滿的
+            string error = process.StandardError.ReadToEnd();
+
+            //bool containsSearchResult = output.Contains("WinFormsTagDownload");
+            ////bool containsSearchResult = output.Contains("WinFormsTagDownload");
+            //Console.WriteLine($"Contains \"WinFormsTagDownload\"? {containsSearchResult}");
+
+
+            exitCode = process.ExitCode;
+            if (output.Contains("Reset type NORMAL:"))
+            {
+                UnityEngine.Debug.Log("Can connect to target..........");
+
+            }
+            else
+            {
+                exitCode = 2;
+                error = "ExecuteCommandTest Fail....";
+                string message = String.Format("error = {0} \r\nexitCode = {1}", (String.IsNullOrEmpty(error) ? "(none)" : error), exitCode);
+                UnityEngine.Debug.Log("Cannot connect to target....");
+                EditorUtility.DisplayDialog("DialogMsg", message, "Yes");
+                //MessageBox.Show(message, "Show...", buttons);
+            }
+
+            //string message = String.Format("error = {0} \r\nexitCode = {1}", (String.IsNullOrEmpty(error) ? "(none)" : error), exitCode);
+            UnityEngine.Debug.Log("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+            UnityEngine.Debug.Log("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+            UnityEngine.Debug.Log("ExitCode: " + exitCode.ToString() + "ExecuteCommand");
+            //MessageBox.Show(message, "Show...", buttons);
+            process.Close();
+
+            return exitCode;
+        }
+
+        /*
+        static void Main()
+        {
+            ExecuteCommand("echo testing");
+        }
+        */
+
+
+
+    }
+}
---
+++ Assets/Scripts/Download.cs
@@ -0,0 +1,356 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
+//using System.Windows.Forms;
+using WinFormsTagDownload;
+
+
+public class Download : MonoBehaviour
+{
+    public InputField   textTest;
+    public InputField[] tbx_id;
+    public InputField   tbx_TempRef;
+    public InputField tbx_LSMin;
+    public InputField tbx_LSHour;
+    public InputField tbx_RfChannel;
+
+    public Dropdown mDropWakeUpSec;
+    public Dropdown mDropRptCnt;
+    public Dropdown mDropMaxCnt;
+
+
+    int score, cnt;
+    int mDropWakeUpSecValue;
+
+
+    private byte[] readBin;
+    private const int TEMPERATURE_NVM_BASE_ADDR = 0x3EE80;
+    private const int NVM_BASE_START_ADDR = 0x3EF00;
+    private const int ACTIVE_NVM_BASE_ADDR = 0x3EF30;
+
+    private const int NVM_BASE_TAGID0_ADDR = 0x3EF02;
+    private const int NVM_BASE_TAGID1_ADDR = 0x3EF03;
+    private const int NVM_BASE_TAGID2_ADDR = 0x3EF04;
+    private const int NVM_BASE_TAGID3_ADDR = 0x3EF05;
+    private const int NVM_BASE_TAGID4_ADDR = 0x3EF06;
+
+    private const int NVM_BASE_RfChn_ADDR = 0x3EF08;
+    private const int NVM_BASE_WakeUp_ADDR = 0x3EF09;
+    private const int NVM_BASE_LSMin_ADDR = 0x3EF0A;
+    private const int NVM_BASE_LSHour_ADDR = 0x3EF0B;
+    private const int NVM_BASE_MaxRetryCount_ADDR = 0x3EF1F;
+    private const int NVM_BASE_AdcBase_ADDR = 0x3EF20;
+    private const int NVM_BASE_Reserved_ADDR = 0x3EF22;
+
+    private const int NVM_BASE_ReportCnt_ADDR = 0x3EF32;
+    private const int NVM_BASE_TempRef_ADDRL = 0x3EE86;
+    private const int NVM_BASE_TempRef_ADDRH = 0x3EE87;
+
+    private TClassUtility ClassUtility;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClassUtility = new TClassUtility();
+        Debug.Log("Start()!!!!!!");
+        textTest.text = "JLinkWrite.bat";
+        BinaryFileRead();
+    }
+
+    void Update()
+    {
+        //Debug.Log("Update()!!!!!!... " + cnt++);
+    }
+
+    public void ExitButton()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(0);
+
+    }
+
+
+    public void btn_tabP1_Burn1_Click()
+    {
+        int retcode;
+
+        try
+        {
+
+            retcode = ClassUtility.ExecuteCommandTest("dir");
+            //retcode = ClassUtility.ExecuteCommandTest("JLinkConnectTest.bat");
+            if (retcode != 0)
+            {
+                Debug.Log("Down load Fail.... ");
+                //toolStripStatusLabel1.Text = "Down load Fail......";
+                //statusStrip1.Update();
+            }
+            else
+            {
+                ClassUtility.ExecuteCommand("dir");
+                //ClassUtility.ExecuteCommand(textBox1.Text);
+                //toolStripStatusLabel1.Text = "Down load success......";
+                //statusStrip1.Update();
+                Debug.Log("Down load success.... ");
+            }
+
+        }
+        catch (IOException e)
+        {
+            //MessageBoxButtons buttons = MessageBoxButtons.OK;
+            Debug.Log("\nException Caught!");
+            //Debug.Log("Message :{0} ", e.Message);
+            Debug.Log(e.Message + " Cannot read from file.");
+            EditorUtility.DisplayDialog("DialogMsg", e.Message, "Yes");
+
+            return;
+        }
+
+    }
+
+
+
+    public void btn_tabP1_Burn2_Click()
+    {
+        int retcode;
+        retcode = ClassUtility.ExecuteCommandTest("JLinkConnectTest.bat");
+        if (retcode != 0)
+        {
+
+            Debug.Log("JLinkConnectTest Fail.... ");
+            //toolStripStatusLabel1.Text = "Down load Fail......";
+            //statusStrip1.Update();
+            return;
+        }
+
+        byte[] TAG_ID = new byte[5];
+        TAG_ID[1] = ClassUtility.HexToByte(tbx_id[1].text);      //tbx_id4.Text
+        TAG_ID[2] = ClassUtility.HexToByte(tbx_id[2].text);      //tbx_id4.Text
+        TAG_ID[3] = ClassUtility.HexToByte(tbx_id[3].text);      //tbx_id4.Text
+        TAG_ID[4] = ClassUtility.HexToByte(tbx_id[4].text);      //tbx_id4.Text
+
+        TAG_ID[4] = (byte)(TAG_ID[4] + 1);
+
+        if (((TAG_ID[4]) & 0xFF) == 0xFF)
+        {
+            TAG_ID[4] = 0x01;
+
+            if (TAG_ID[3] < 0xFF)
+            {
+                TAG_ID[3] = (byte)(TAG_ID[3] + 1);
+                tbx_id[3].text = TAG_ID[3].ToString("X2");
+            }
+            else if (TAG_ID[3] == 0xFF)
+            {
+                TAG_ID[3] = 0x00;
+                TAG_ID[2] = (byte)(TAG_ID[2] + 1);
+                tbx_id[3].text = TAG_ID[3].ToString("X2");
+                tbx_id[2].text = TAG_ID[2].ToString("X2");
+
+            }
+        }
+        tbx_id[4].text = TAG_ID[4].ToString("X2");
+
+
+        TAG_ID[0] = (byte)((TAG_ID[1] + TAG_ID[2] + TAG_ID[3] + ((TAG_ID[4]) & 0xFF)) % 255);
+        tbx_id[0].text = TAG_ID[0].ToString("X2");
+        Debug.Log("TAG_ID:" + TAG_ID[0].ToString("X2") + TAG_ID[1].ToString("X2") + TAG_ID[2].ToString("X2") + TAG_ID[3].ToString("X2") + TAG_ID[4].ToString("X2"));
+
+        btn_tabP1_SaveFile_Click();
+
+        btn_tabP1_Burn1_Click();
+
+    }
+
+
+    public void TestButton()
+    {
+        //string m_Message;
+        Debug.Log("textTest: " + textTest.text);
+
+        //textTest.text = score.ToString();
+        //Debug.Log(score);
+
+        mDropWakeUpSecValue = mDropWakeUpSec.value;
+        Debug.Log("WakeUpSec = " + mDropWakeUpSecValue);
+
+        //m_Message = mDropWakeUpSec.options[score].text;
+        //Debug.Log("m_Message = " + m_Message);
+
+        //mDropWakeUpSec.captionText.text = mDropWakeUpSec.options[score].text;
+        Debug.Log("mDropWakeUpSec = " + mDropWakeUpSec.captionText.text);
+        Debug.Log("mDropRptCnt = " + mDropRptCnt.captionText.text);
+        Debug.Log("mDropMaxCnt = " + mDropMaxCnt.captionText.text);
+
+        score++;
+        btn_tabP1_SaveFile_Click();
+    }
+
+    private void BinaryFileRead()
+    {
+        //byte[] ByteArray = { 84};
+        //byte[] ByteArray = { 84, 104, 105, 115, 32, 105, 115, 32, 101, 120, 97, 109, 112, 108, 101, };
+        byte[] ByteArray = new byte[1];
+        //byte[] ByteArray2 = new byte[2];
+        ushort TempRef;
+        double dTempRef;
+        List<byte> byteSource = new List<byte>();
+
+        try
+        {
+            Debug.Log("BinaryFileRead()");
+            string path = @"hex.bin";
+            // Calling the ReadAllBytes() function
+            readBin = File.ReadAllBytes(path);
+            byteSource.AddRange(readBin);
+            //Debug.Log("byteSource len:      {0}", byteSource.Count.ToString("X8"));
+
+            //Console.WriteLine("byteSource List<byte>: {0}", byteSource[NVM_BASE_TAGID0_ADDR].ToString("X2"));
+
+            /*
+            Buffer.BlockCopy(readBin, NVM_BASE_TAGID0_ADDR, ByteArray, 0, 1);
+            tbx_id0.Text = ClassUtility.ToHexString(ByteArray);
+
+            Buffer.BlockCopy(readBin, NVM_BASE_TAGID1_ADDR, ByteArray, 0, 1);
+            tbx_id1.Text = ClassUtility.ToHexString(ByteArray);
+
+            Buffer.BlockCopy(readBin, NVM_BASE_TAGID2_ADDR, ByteArray, 0, 1);
+            tbx_id2.Text = ClassUtility.ToHexString(ByteArray);
+
+            Buffer.BlockCopy(readBin, NVM_BASE_TAGID3_ADDR, ByteArray, 0, 1);
+            tbx_id3.Text = ClassUtility.ToHexString(ByteArray);
+
+            Buffer.BlockCopy(readBin, NVM_BASE_TAGID4_ADDR, ByteArray, 0, 1);
+            tbx_id4.Text = ClassUtility.ToHexString(ByteArray);
+            */
+
+            tbx_id[0].text = readBin[NVM_BASE_TAGID0_ADDR].ToString("X2");
+            tbx_id[1].text = readBin[NVM_BASE_TAGID1_ADDR].ToString("X2");
+            tbx_id[2].text = readBin[NVM_BASE_TAGID2_ADDR].ToString("X2");
+            tbx_id[3].text = readBin[NVM_BASE_TAGID3_ADDR].ToString("X2");
+            tbx_id[4].text = readBin[NVM_BASE_TAGID4_ADDR].ToString("X2");
+
+            tbx_RfChannel.text = readBin[NVM_BASE_RfChn_ADDR].ToString();
+            //tbx_WakeUp.Text = readBin[NVM_BASE_WakeUp_ADDR].ToString();
+            //comboBox1.Text = readBin[NVM_BASE_WakeUp_ADDR].ToString();
+
+            tbx_LSMin.text = readBin[NVM_BASE_LSMin_ADDR].ToString();
+            tbx_LSHour.text = readBin[NVM_BASE_LSHour_ADDR].ToString();
+
+            //tbx_RpCnt.Text = readBin[NVM_BASE_ReportCnt_ADDR].ToString();
+            //cbx_ReportCnt.Text = readBin[NVM_BASE_ReportCnt_ADDR].ToString();
+            //cbx_RetryCnt.Text = readBin[NVM_BASE_MaxRetryCount_ADDR].ToString();
+
+            //Buffer.BlockCopy(readBin, NVM_BASE_TempRef_ADDR, ByteArray2, 0, 2);
+
+            mDropWakeUpSec.captionText.text = readBin[NVM_BASE_WakeUp_ADDR].ToString();
+            mDropRptCnt.captionText.text = readBin[NVM_BASE_ReportCnt_ADDR].ToString();
+            mDropMaxCnt.captionText.text = readBin[NVM_BASE_MaxRetryCount_ADDR].ToString();
+
+            TempRef = (ushort)(readBin[NVM_BASE_TempRef_ADDRH] << 8 | readBin[NVM_BASE_TempRef_ADDRL]);
+            dTempRef = TempRef / 10.0;
+            string message = String.Format("TempRef = {0}", dTempRef);
+            Debug.Log(message);
+            Debug.Log("AAAA___TempRef =" + dTempRef );
+            tbx_TempRef.text = dTempRef.ToString("00.0");
+
+            /*             Buffer.BlockCopy(readBin, NVM_BASE_TempRef_ADDR, ByteArray2, 0, 2);
+                         TempRef = (ushort)(ByteArray2[1] << 8 | ByteArray2[0]);
+                         dTempRef = TempRef / 10.0;
+                         Console.WriteLine("TempRef:      {0}", dTempRef);
+                         tbx_TempRef.Text = dTempRef.ToString();    */
+
+            //ClassUtility.DumpBytes(ByteArray, ByteArray.Length);
+            Debug.Log("BinaryFileRead() ... end");
+        }
+        catch (IOException e)
+        {
+            //MessageBoxButtons buttons = MessageBoxButtons.OK;
+            Debug.Log("\nException Caught!");
+            //Debug.Log("Message :{0} ", e.Message);
+            Debug.Log(e.Message + " Cannot read from file.");
+            EditorUtility.DisplayDialog("DialogMsg", e.Message, "Yes");
+
+            return;
+        }
+    }
+
+    private void BinaryFileWrite()
+    {
+        try
+        {
+            Debug.Log("BinaryFileWrite()");
+            string path = @"hex.bin";
+            // Adding new contents to the file 
+            File.WriteAllBytes(path, readBin);
+            //ClassUtility.DumpBytes(readBin, readBin.Length);
+        }
+        catch (IOException e)
+        {
+            Debug.Log("\nException Caught!");
+            Debug.Log("Message : " + e.Message);
+            Debug.Log(e.Message + " Cannot write from file.");
+            return;
+        }
+    }
+
+
+    private void btn_tabP1_SaveFile_Click()
+    {
+        ushort TempRef;
+        double dTempRef;
+        //byte[] ByteArray = new byte[30];
+
+        readBin[NVM_BASE_TAGID0_ADDR] = ClassUtility.HexToByte(tbx_id[0].text);      //tbx_id0.Text
+        readBin[NVM_BASE_TAGID1_ADDR] = ClassUtility.HexToByte(tbx_id[1].text);      //tbx_id1.Text
+        readBin[NVM_BASE_TAGID2_ADDR] = ClassUtility.HexToByte(tbx_id[2].text);      //tbx_id2.Text
+        readBin[NVM_BASE_TAGID3_ADDR] = ClassUtility.HexToByte(tbx_id[3].text);      //tbx_id3.Text
+        readBin[NVM_BASE_TAGID4_ADDR] = ClassUtility.HexToByte(tbx_id[4].text);      //tbx_id4.Text
+
+
+        readBin[NVM_BASE_RfChn_ADDR] = Convert.ToByte(tbx_RfChannel.text);
+
+        //readBin[NVM_BASE_WakeUp_ADDR] = Convert.ToByte(tbx_WakeUp.Text);
+        readBin[NVM_BASE_WakeUp_ADDR] = Convert.ToByte(mDropWakeUpSec.captionText.text);
+        //Console.WriteLine("comboBox1_SelectedString = {0},  {1}", comboBox1.SelectedItem.ToString(), comboBox1.Text);
+
+        readBin[NVM_BASE_LSMin_ADDR] = Convert.ToByte(tbx_LSMin.text);
+        readBin[NVM_BASE_LSHour_ADDR] = Convert.ToByte(tbx_LSHour.text);
+
+        readBin[NVM_BASE_ReportCnt_ADDR] = Convert.ToByte(mDropRptCnt.captionText.text);
+        //readBin[NVM_BASE_ReportCnt_ADDR] = Convert.ToByte(tbx_RpCnt.Text);
+        //readBin[NVM_BASE_Reserved_ADDR] = ClassUtility.HexToByteArray(tbx_Rev.Text);
+
+        readBin[NVM_BASE_MaxRetryCount_ADDR] = Convert.ToByte(mDropMaxCnt.captionText.text);
+
+
+        dTempRef = Convert.ToDouble(tbx_TempRef.text);
+        TempRef = (ushort)(dTempRef * 10);
+        readBin[NVM_BASE_TempRef_ADDRL] = (byte)(TempRef & 0x0000FF);
+        readBin[NVM_BASE_TempRef_ADDRH] = (byte)((TempRef >> 8) & 0x0000FF);
+
+        //ByteArray = ClassUtility.HexToByteArray(tbx_Rev.Text);
+        //ClassUtility.DumpBytes(ByteArray, ByteArray.Length);
+
+        //Debug.Log("Message :{0} ", ClassUtility.ByteArrayToHexString(readBin[NVM_BASE_ReportCnt_ADDR]);
+        Debug.Log("button1_Click()..................");
+        BinaryFileWrite();
+
+        //byte[] byteArray = System.Text.Encoding.Default.GetBytes(tbx_Rev.Text);
+        //Console.WriteLine("byteArray.Length: {0}", byteArray.Length);
+
+        //System.Text.Encoding.Default.GetBytes(String); //stringÂàbyte[]
+        //System.Text.Encoding.Default.GetString(byte[]);//byte[]ÂàString 
+
+        //ClassUtility.ArrayCopyTestFunction();
+
+
+    }
+
+
+}
